61a956b409ab7905303dd4b7c7d19cae70527727
Kavish Goyal <48274723+KavishGoyal@users.noreply.github.com>
2022-12-08 22:40:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import nltk
 nltk.download("stopwords")
 nltk.download("punkt")
 nltk.download('wordnet')
+nltk.download('omw-1.4')
 from nltk.tokenize import word_tokenize
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
